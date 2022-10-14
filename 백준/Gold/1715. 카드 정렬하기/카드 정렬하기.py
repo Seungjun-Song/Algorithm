90f8f73395e78d75cc57cd73d8c01e021235fb4c
seungjun123 <80227755+seungjun123@users.noreply.github.com>
2022-10-14 18:45:47 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 208 ms, Memory: 33692 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,20 +1,18 @@
-import heapq
-import sys
+import heapq, sys
+input = sys.stdin.readline
 
 N = int(input())
-card_deck = []
-for _ in range(N):
-    heapq.heappush(card_deck, int(sys.stdin.readline()))
+card = list(int(input()) for _ in range(N))
+heapq.heapify(card)
 
-
-if len(card_deck) == 1: #1개일 경우 비교하지 않아도 된다
+if len(card) == 1:
     print(0)
 else:
-    answer = 0
-    while len(card_deck) > 1: #1개일 경우 비교하지 않아도 된다
-        temp_1 = heapq.heappop(card_deck) #제일 작은 덱
-        temp_2 = heapq.heappop(card_deck) #두번째로 작은 덱
-        answer += temp_1 + temp_2 #현재 비교 횟수를 더해줌
-        heapq.heappush(card_deck, temp_1 + temp_2) #더한 덱을 다시 넣어줌
+    cnt = 0
+    while len(card) > 1: #1개일 경우 비교하지 않아도 된다
+        temp_1 = heapq.heappop(card) #제일 작은 덱
+        temp_2 = heapq.heappop(card) #두번째로 작은 덱
+        cnt += temp_1 + temp_2 #현재 비교 횟수를 더해줌
+        heapq.heappush(card, temp_1 + temp_2) #더한 덱을 다시 넣어줌
     
-    print(answer)
+    print(cnt)
