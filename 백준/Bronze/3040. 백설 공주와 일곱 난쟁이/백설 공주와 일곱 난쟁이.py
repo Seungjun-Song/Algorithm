0792c6136c347e25232ce888467e610a26fbe474
seungjun123 <80227755+seungjun123@users.noreply.github.com>
2022-10-18 17:59:29 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -0,0 +1,12 @@
+num = [int(input()) for _ in range(9)]
+
+for i in range(8):
+    for j in range(i+1, 9):
+        if sum(num) - num[i] - num[j] == 100:
+            x, y = i, j
+            break
+
+num.pop(x)
+num.pop(y-1)
+for n in num:
+    print(n)
