01f1d855276517ba18eb7347c1f34c7b6cca0108
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-11-12 22:30:29 +0900

    [Bronze II] Title: 분해합, Time: 1460 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for i in range(1, N+1): # N+1 -> N
+    A = list(map(int, str(i)))
+    M = i + sum(A)
+    if(M == N):
+        print(i)
+        break
+    if(i == N):
+        print(0)
