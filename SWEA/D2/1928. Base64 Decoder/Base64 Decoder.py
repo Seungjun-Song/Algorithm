c0377853a095468dce15d21ec53496e8cbaa3e53
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-11-18 05:54:43 +0900

    [D2] Title: Base64 Decoder, Time: 90 ms, Memory: 45,432 KB -BaekjoonHub

---
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.py
@@ -0,0 +1,25 @@
+T = int(input())
+for test in range(1, T+1):
+    word = list(input())
+    length = len(word)
+    decode = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+          'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+          '0','1','2','3','4','5','6','7','8','9','+','/'
+          ]
+    
+    res = ''
+    for q in range(length):
+        num = decode.index(word[q])
+        bin_num = str(bin(num)[2:])
+ 
+        while len(bin_num) < 6:
+            bin_num = '0' + bin_num
+ 
+        res += bin_num
+    
+    r = ''
+    for w in range(length*6//8):
+        e = int(res[w*8:w*8+8],2)
+        r += chr(e)
+ 
+    print('#%d %s' % (test, r))
