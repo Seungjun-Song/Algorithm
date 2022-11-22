fbea1533d3cafe684e336dbbb00a25b27eb7c086
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-11-22 20:14:20 +0900

    [level 1] Title: 모의고사, Time: 4.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,49 +1,21 @@
 def solution(answers):
     answer = []
     
-    first = []
-    for i in range(1, 10001):
-        if i % 5 == 0:
-            first.append(5)
-        else:
-            first.append(i % 5)
-        
-    second = []
-    temp = [1, 3, 4, 5]
-    for i in range(10000):
-        if i % 2 == 0:
-            second.append(2)
-        else:
-            second.append(temp[(i % 8) // 2])
-            
-    third = []
-    temp = [3, 1, 2, 4, 5]
-    for i in range(0, 10000, 2):
-        for _ in range(2):
-            third.append(temp[(i % 10) // 2])
-        
-    one, two, three = 0, 0, 0    
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 1, 2, 4, 5]
+
+    cnt = [0, 0, 0]
     for i in range(len(answers)):
-        if first[i] == answers[i]:
-            one += 1
-        if second[i] == answers[i]:
-            two += 1
-        if third[i] == answers[i]:
-            three += 1
-    
-    if one == two == three:
-        answer = [1, 2, 3]
-    elif one == two and one > three:
-        answer = [1, 2]
-    elif one == three and one > two:
-        answer = [1, 3]
-    elif two == three and two > one:
-        answer = [2, 3]
-    elif one > two and one > three:
-        answer = [1]
-    elif two > one and two > three:
-        answer = [2]
-    elif three > one and three > two:
-        answer = [3]
+        if answers[i] == first[i % 5]:
+            cnt[0] += 1
+        if answers[i] == second[i % 8]:
+            cnt[1] += 1
+        if answers[i] == third[(i // 2) % 5]:
+            cnt[2] += 1
+        
+    for i in range(len(cnt)):
+        if max(cnt) == cnt[i]:
+            answer.append(i + 1)
     
     return answer
