1102cb14219bd67337654554b59c2695f593e8c0
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-11-28 17:58:26 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -2,12 +2,12 @@ N, K = map(int, input().split())
 arr = [i for i in range(1, N+1)]
 
 answer = []
-num = 0
-
+idx = 0
 for i in range(N):
-    num += K-1
-    if num >= len(arr):
-        num = num%len(arr)
+    idx += K - 1
+    if idx >= len(arr):
+        idx %= len(arr)
+    
+    answer.append(str(arr.pop(idx)))
 
-    answer.append(str(arr.pop(num)))
-print("<", ", ".join(answer), ">", sep='')
+print("<{}>".format(", ".join(answer)))
