8cfd0c25d24e07ef3e8e401cd9f66fe035261f53
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-12-03 19:04:28 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 76 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -1,25 +1,21 @@
-# 1011번
-
 import math
-
 T = int(input())
-
 for _ in range(T):
     x, y = map(int, input().split())
-    distance = y - x
-    count = 0
+    distance = y-x
+    cnt = 0
 
-    num = math.floor(math.sqrt(distance))   # n제곱 <= 거리 < (n+1)제곱일때 n제곱
-    num_square = num ** 2   # n제곱의 제곱
+    num_square = math.floor(math.sqrt(distance))
+    num = num_square ** 2
 
-    if distance > num_square + num:
-        count = 2 * num + 1
-    elif distance == num_square:
-        count = 2 * num - 1
-    elif distance <= num_square + num:
-        count = 2 * num
+    if distance == num:
+        cnt = 2 * num_square - 1
+    elif distance <= num + num_square:
+        cnt = 2 * num_square
+    else:
+        cnt = 2 * num_square + 1
 
     if distance < 4:
-        count = distance
-
-    print(count)
+        cnt = distance
+        
+    print(cnt)
