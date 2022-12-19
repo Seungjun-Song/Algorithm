7407c58eaf6c375967c8f25a4a67dcb7ee7cfe0f
seungjun <80227755+seungjun123@users.noreply.github.com>
2022-12-19 16:22:17 +0900

    [Silver IV] Title: 바닥 장식, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,22 +1,18 @@
 import sys
 sys.setrecursionlimit(10000)
 
-def dfs(x, y):
-    dx = [1, -1]
-    dy = [1, -1]
-    
-    temp = arr[x][y]
-    arr[x][y] = 0
-    
-    if temp == '-':
-        for i in range(2):
-            ny = y + dy[i]
+def dfs(x, y):    
+    if arr[x][y] == '-':
+        arr[x][y] = 0
+        for i in [1, -1]:
+            ny = y + i
             if 0 <= ny < M and arr[x][ny] == '-':
                 dfs(x, ny)    
     
-    if temp == '|':
-        for i in range(2):
-            nx = x + dx[i]
+    if arr[x][y] == '|':
+        arr[x][y] = 0
+        for i in [1, -1]:
+            nx = x + i
             if 0 <= nx < N and arr[nx][y] == '|':
                 dfs(nx, y)
 
