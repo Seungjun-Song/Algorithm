d23443cf963c51609987d294f04965bab32fadf6
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-02-05 22:55:55 +0900

    [Silver I] Title: 나이트의 이동, Time: 1352 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -3,11 +3,11 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(1000000)
 
-dx = [-1, 1, 2, 2, 1, -1, -2, -2]
-dy = [2, 2, 1, -1, -2, -2, -1, 1]
+dx = [-2, -1, 1, 2, 2, 1, -1, -2]
+dy = [1, 2, 2, 1, -1, -2, -2, -1]
 def bfs():
     q = deque()
-    q.append((startX, startY))
+    q.append([startX, startY])
     while q:
         x, y = q.popleft()
         if x == endX and y == endY:
@@ -18,12 +18,12 @@ def bfs():
             ny = y + dy[i]
             if 0 <= nx < l and 0 <= ny < l and graph[nx][ny] == 0:
                 graph[nx][ny] = graph[x][y] + 1
-                q.append((nx,ny))
-                
+                q.append([nx, ny])
+
 T = int(input())
 for _ in range(T):
     l = int(input())
     startX, startY = map(int, input().split())
     endX, endY = map(int, input().split())
-    graph = [[0]*l for _ in range(l)]
+    graph = [[0] * l for _ in range(l)]
     print(bfs())
