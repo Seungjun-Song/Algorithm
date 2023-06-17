3382748347fe02dada8579ba3a534fb0ffed0e69
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-06-17 15:32:15 +0900

    [Silver II] Title: 색종이 만들기, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,21 +1,23 @@
-def confetti(N, x, y):
+N = int(input())
+arr = [list(input().split()) for _ in range(N)]
+color = [0, 0]
+
+def dfs(x, y, N):
+    check = arr[x][y]
     for i in range(x, x+N):
         for j in range(y, y+N):
-            if array[i][j] != array[x][y]:
-                confetti(N//2, x, y)
-                confetti(N//2, x+N//2, y)
-                confetti(N//2, x, y+N//2)
-                confetti(N//2, x+N//2, y+N//2)
+            if check != arr[i][j]:
+                dfs(x, y, N//2)
+                dfs(x, y+N//2, N//2)
+                dfs(x+N//2, y, N//2)
+                dfs(x+N//2, y+N//2, N//2)
                 return
     
-    if array[x][y] == 0:
-        paper[0] += 1
+    if check == '0':
+        color[0] += 1
     else:
-        paper[1] += 1                
-
-N = int(input())
-array = [list(map(int, input().split())) for _ in range(N)]
-paper = [0, 0]
-confetti(N, 0, 0)
-print(paper[0])
-print(paper[1])
+        color[1] += 1
+    
+dfs(0, 0, N)
+for i in color:
+    print(i)
