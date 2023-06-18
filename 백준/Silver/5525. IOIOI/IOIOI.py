312598da95380bd589b1bd8d7321671394038c50
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-06-18 16:34:39 +0900

    [Silver I] Title: IOIOI, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,13 @@
+N = int(input())
+M = int(input())
+S = input()
+
+Pn = 'I'
+for _ in range(N):
+    Pn += 'OI'
+
+cnt = 0
+for i in range(M-N*2):
+    if Pn in S[i:i+N*2+1]:
+        cnt += 1
+print(cnt)
