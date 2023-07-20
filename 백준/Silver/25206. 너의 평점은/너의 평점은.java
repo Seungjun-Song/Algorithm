db392ad7a4f5528abebbf29a620ac3e62d05127f
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-07-20 17:25:53 +0900

    [Silver V] Title: 너의 평점은, Time: 136 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,52 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 학점 * 과목평점
-        double aa = 0;
-        // 총학점
-        double point = 0;
-
-        for (int i = 0; i < 20; i++) {
-            // 과목
-            String title = scanner.next();
-            // 학점
-            double a = scanner.nextDouble();
-            // 평점
-            String b = scanner.next();
-
-            if (b.equals("A+")){
-                aa += a * 4.5;
-                point += a;
-            } else if (b.equals("A0")) {
-                aa += a * 4.0;
-                point += a;
-            }else if (b.equals("B+")) {
-                aa += a * 3.5;
-                point += a;
-            }else if (b.equals("B0")) {
-                aa += a * 3.0;
-                point += a;
-            }else if (b.equals("C+")) {
-                aa += a * 2.5;
-                point += a;
-            }else if (b.equals("C0")) {
-                aa += a * 2.0;
-                point += a;
-            }else if (b.equals("D+")) {
-                aa += a * 1.5;
-                point += a;
-            }else if (b.equals("D0")) {
-                aa += a * 1.0;
-                point += a;
-            }else if (b.equals("F")) {
-                aa += a * 0.0;
-                point += a;
-            }
-        }
-
-            System.out.printf("%.6f", aa / point);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double gradeSum = 0;
+		double answer = 0;
+		
+		for(int i=0; i<20; i++) {
+			String[] s = br.readLine().split(" ");
+			double credit = Double.parseDouble(s[1]);
+			String grade = s[2];
+			if(grade.equals("P")) continue;
+			gradeSum += credit;
+			
+			if(grade.equals("A+")) credit *= 4.5;
+			else if(grade.equals("A0")) credit *= 4.0;
+			else if(grade.equals("B+")) credit *= 3.5;
+			else if(grade.equals("B0")) credit *= 3.0;
+			else if(grade.equals("C+")) credit *= 2.5;
+			else if(grade.equals("C0")) credit *= 2.0;
+			else if(grade.equals("D+")) credit *= 1.5;
+			else if(grade.equals("D0")) credit *= 1.0;
+			else if(grade.equals("F")) credit *= 0.0;
+			
+			
+			answer += credit;
+		}
+		System.out.printf("%.6f", (answer / gradeSum));
+	}
 }
