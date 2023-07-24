95edb0a65e632d486c44fe610cb949e0fd0b8040
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-07-24 22:36:13 +0900

    [Gold IV] Title: 문자열 폭발, Time: 620 ms, Memory: 62044 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String s = br.readLine();
@@ -13,12 +13,14 @@ public class Main {
 		Stack<Character> stack = new Stack<>();
 		
 		for(int i=0; i<s.length(); i++) {
-			stack.push(s.charAt(i));
-			boolean flag = true;
+			stack.add(s.charAt(i));
 			if(stack.size() >= boom.length()) {
+				boolean flag = true;
 				for(int j=0; j<boom.length(); j++) {
-					if(stack.get(stack.size()-boom.length()+j) != boom.charAt(j))
+					if(stack.get(stack.size()-boom.length()+j) != boom.charAt(j)) {
 						flag = false;
+						break;
+					}
 				}
 				if(flag) {
 					for(int k=0; k<boom.length(); k++) {
@@ -31,8 +33,9 @@ public class Main {
 		if(stack.isEmpty()) System.out.println("FRULA");
 		else {
 			StringBuilder sb = new StringBuilder();
-			while(!stack.isEmpty())
+			while(!stack.isEmpty()) {
 				sb.append(stack.pop());
+			}
 			System.out.println(sb.reverse());
 		}
 	}
