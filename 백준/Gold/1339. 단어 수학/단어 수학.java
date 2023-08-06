e31d4759dde10546b30678ecab01aa488a247ab5
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-08-06 15:14:00 +0900

    [Gold IV] Title: 단어 수학, Time: 84 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,43 +1,35 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N;
+	static int alpha[];
 
 	public static void main(String args[]) throws IOException{
-    	
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         
-        int N = Integer.parseInt(br.readLine());
-        int alpha[] = new int[26];
+        N = Integer.parseInt(st.nextToken());
+        alpha = new int[26];
         
         for(int i=0; i<N; i++) {
         	String s = br.readLine();
-        	double x = Math.pow(10, s.length()-1);
         	for(int j=0; j<s.length(); j++) {
-        		alpha[s.charAt(j) - 'A'] += x;
-        		x /= 10;
+        		alpha[s.charAt(j)-'A'] += Math.pow(10, s.length()-j-1);
         	}
         }
         
-        Integer[] arrAlpha = Arrays.stream(alpha).boxed().toArray(Integer[]::new);
-        Arrays.sort(arrAlpha, Collections.reverseOrder());
-        int cnt = 9;
-        int sum = 0;
-        for(Integer i : arrAlpha) {
-        	sum += i*cnt--;
+        Arrays.sort(alpha);
+        int cnt = 9, sum = 0;
+        for(int i=25; i>-1; i--) {
+        	if(alpha[i] != 0) sum += alpha[i]*cnt--; 
         }
+        
         System.out.println(sum);
     }
 }
-
-// 자릿수가 큰 것 ~ 자릿수가 작은 것 => 9 ~ 0
-// 길이가 같을 때는 
