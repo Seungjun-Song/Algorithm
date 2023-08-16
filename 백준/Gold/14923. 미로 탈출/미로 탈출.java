f59a58e7fb4b503bb439e73291da75642fabb950
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-08-17 03:52:05 +0900

    [Gold IV] Title: 미로 탈출, Time: 628 ms, Memory: 121192 KB -BaekjoonHub

--- 백준/Gold/14923. 미로 탈출/미로 탈출.java
+++ 백준/Gold/14923. 미로 탈출/미로 탈출.java
@@ -7,8 +7,8 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, Hx, Hy, Ex, Ey;
-	static int[][] arr;
+	static int N, M, Hx, Hy, Ex, Ey, answer=-1;
+	static int[][] map;
 	static boolean[][][] visited;
 	static int[] dx = {0, 1, 0, -1};
 	static int[] dy = {1, 0, -1, 0};
@@ -27,49 +27,48 @@ public class Main {
 		Ex = Integer.parseInt(st.nextToken());
 		Ey = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N+1][M+1];
+		map = new int[N+1][M+1];
 		visited = new boolean[N+1][M+1][2];
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=1; j<=M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
 		bfs(Hx, Hy, Ex, Ey);
 	}
-
+	
 	static void bfs(int x, int y, int endX, int endY) {
 		Queue<node> q = new LinkedList<>();
 		q.add(new node(x, y, 1, 0));
 		visited[x][y][1] = true;
-		int answer = -1;
-		
 		while(!q.isEmpty()) {
 			node no = q.poll();
-			int curX = no.x;
-			int curY = no.y;
-			int cnt = no.cnt;
-			if(curX==endX && curY==endY) {
+			
+			if(no.x==endX && no.y==endY) {
 				answer = no.ans;
 				break;
 			}
 			
+			int nx, ny;
 			for(int i=0; i<4; i++) {
-				int nx = curX + dx[i];
-				int ny = curY + dy[i];
+				nx = no.x + dx[i];
+				ny = no.y + dy[i];
+				
 				if(1>nx || nx>N || 1>ny || ny>M) continue;
-				if(cnt>0 && !visited[nx][ny][cnt-1] && arr[nx][ny]==1) {
-					visited[nx][ny][cnt-1] = true;
-					q.add(new node(nx, ny, cnt-1, no.ans+1));
+				
+				if(no.cnt>0 && !visited[nx][ny][no.cnt-1] && map[nx][ny]==1) {
+					visited[nx][ny][no.cnt-1] = true;
+					q.add(new node(nx, ny, no.cnt-1, no.ans+1));
 				}
-				if(!visited[nx][ny][cnt] && arr[nx][ny] == 0) {
-					visited[nx][ny][cnt] = true;
-					q.add(new node(nx, ny, cnt, no.ans+1));
+				
+				if(!visited[nx][ny][no.cnt] && map[nx][ny]==0) {
+					visited[nx][ny][no.cnt] = true;
+					q.add(new node(nx, ny, no.cnt, no.ans+1));
 				}
 			}
 		}
-		
 		System.out.println(answer);
 	}
 	
