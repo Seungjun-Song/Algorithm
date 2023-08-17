032a0b975e8ad047a7679a01ac76587bfb814837
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-08-18 01:15:35 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 84 ms, Memory: 11740 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,74 +1,70 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, arr[][];
+	static int N, M;
+	static int map[][];
 	static boolean visited[];
-	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        arr = new int[N+1][N+1];
-        visited = new boolean[N+1];
-        for(int i=0; i<M; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int x = Integer.parseInt(st.nextToken());
-        	int y = Integer.parseInt(st.nextToken());
-        	arr[x][y] = arr[y][x] = 1;
-        }
-        
-        int MV = Integer.MAX_VALUE;
-        int answer = 0;
-        for(int i=1; i<=N ;i++) {
-        	visited = new boolean[N+1];
-        	int x = bfs(i, 1);
-        	if(MV>x) {
-        		MV = x;
-        		answer = i;
-        	}
-        }
-        System.out.println(answer);
-    }
-
-	static int bfs(int idx, int cnt) {
-		Queue<node> q = new LinkedList<>();
-		q.add(new node(idx, 0));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N+1][N+1];
+		visited = new boolean[N+1];
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			map[a][b] = map[b][a] = 1;
+		}
+		
+		int MV = Integer.MAX_VALUE, answer = 0;
+		for(int i=1; i<=N; i++) {
+			visited = new boolean[N+1];
+			int x = bfs(i, 0);
+			if(MV>x) {
+				MV = x;
+				answer = i;
+			}
+		}
+		System.out.println(answer);
+	}
+	
+	static int bfs(int idx, int count) {
+		Queue<node> q = new ArrayDeque<>();
+		q.offer(new node(idx, count));
 		visited[idx] = true;
 		while(!q.isEmpty()) {
 			node no = q.poll();
-			cnt += no.y;
+			count += no.cnt;
 			
 			for(int i=1; i<=N; i++) {
-				int num = arr[no.x][i];
-				
-				if(num==1 && !visited[i]) {
+				if(!visited[i] && map[no.i][i]==1) {
 					visited[i] = true;
-					cnt+=1;
-					q.offer(new node(i, no.y+1));
+					q.offer(new node(i, no.cnt+1));
 				}
 			}
 		}
-		return cnt;
+		return count;
 	}
 	
 	static class node {
-		int x, y;
+		int i, cnt;
 
-		public node(int x, int y) {
+		public node(int i, int cnt) {
 			super();
-			this.x = x;
-			this.y = y;
+			this.i = i;
+			this.cnt = cnt;
 		}
 	}
 }
