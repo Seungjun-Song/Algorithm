32499d92e52481ec028a758485d874682cbeb7f0
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-08-30 10:57:56 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 16828 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,21 +7,15 @@ public class Main {
 		
 		int X = sc.nextInt();
 		
-		int dp[] = new int[1000001];
+		int dp[] = new int[X+1];
 		dp[1] = 0;
-		dp[2] = 1;
-		dp[3] = 1;
-		for(int i=4; i<dp.length; i++) {
-			dp[i] = Integer.MAX_VALUE;
-		}
 		
-		for(int i=4; i<=1000000; i++) {
+		for(int i=2; i<=X; i++) {
+			dp[i] = dp[i-1] + 1;
 			if(i%3==0) dp[i] = Math.min(dp[i/3] + 1, dp[i]);
 			if(i%2==0) dp[i] = Math.min(dp[i/2] + 1, dp[i]);
-			dp[i] = Math.min(dp[i], dp[i-1] + 1);
 		}
 		
 		System.out.println(dp[X]);
 	}
-
 }
