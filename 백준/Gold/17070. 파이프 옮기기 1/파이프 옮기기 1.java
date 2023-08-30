7e6afd84103b3562d377672ea0348a766f020ae9
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-08-30 15:40:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 80 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,104 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, answer=0;
-	static int map[][];
-	static int dx[] = {0, 1, 1};
-	static int dy[] = {1, 0, 1};
-	
-	public static void main(String args[]) throws IOException{
+    static int N;
+    static int[][] input;
+    static int[][][] dp;
+    
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         
         N = Integer.parseInt(br.readLine());
-        map = new int[N+1][N+1];
+        
+        input = new int[N+1][N+1];
+        dp = new int[3][N+1][N+1]; 
+        
         for(int i=1; i<=N; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j=1; j<=N; j++) {
-        		map[i][j] = Integer.parseInt(st.nextToken());
-        	}
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=N; j++) {
+                input[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
         
-        dfs(1, 2, 0);
-        System.out.println(answer);
-    }
-	
-	static void dfs(int x, int y, int direction) {
-		if(x==N && y==N) {
-			answer++;
-			return;
-		}
-		
-		node no;
-		if(direction==0) { // 가로
-			no = H(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-			no = D(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-		} else if(direction==1) { // 세로
-			no = V(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-			no = D(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-		} else { // 대각선
-			no = H(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-			no = V(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-			no = D(x, y);
-			if(no!=null)
-				dfs(no.x, no.y, no.d);
-		}
-	}
-	
-	static node H(int x, int y) { // 가로 이동
-		int nx = x;
-		int ny = y+1;
-		
-		if(1<=nx && nx<=N && 1<=ny && ny<=N && map[nx][ny]==0) {
-			return new node(nx, ny, 0);
-		}
-		return null;
-	}
-	
-	static node V(int x, int y) { // 세로 이동
-		int nx = x+1;
-		int ny = y;
-		
-		if(1<=nx && nx<=N && 1<=ny && ny<=N && map[nx][ny]==0) {
-			return new node(nx, ny, 1);
-		}
-		return null;
-	}
-	
-	static node D(int x, int y) { // 대각선 이동
-		int nx = x+1;
-		int ny = y+1;
-		
-		if(1<=nx && nx<=N && 1<=ny && ny<=N
-				&& map[nx][ny]==0 && map[nx-1][ny]==0 && map[nx][ny-1]==0) {
-			return new node(nx, ny, 2);
-		}
-		return null;
-	}
-	
-	static class node {
-		int x, y, d;
+        dp[0][1][2] = 1;
+        
+        for(int i=1; i<=N; i++) {
+            for(int j=2; j<=N; j++) {
+                //가로로 이동하는 dp
+                if(j<N && input[i][j+1]==0)
+                    dp[0][i][j+1] = dp[0][i][j] + dp[2][i][j];
 
-		public node(int x, int y, int d) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.d = d;
-		}
-	}
+                //세로로 이동하는 dp
+                if(i<N && input[i+1][j]==0)
+                    dp[1][i+1][j] = dp[1][i][j] + dp[2][i][j];
+                
+                //대각선으로 이동하는 dp
+                if(i<N && j<N && input[i+1][j+1]==0 && input[i+1][j]==0 && input[i][j+1]==0)
+                    dp[2][i+1][j+1] = dp[0][i][j] + dp[1][i][j] + dp[2][i][j];
+            }
+        }
+        
+        System.out.println(dp[0][N][N] + dp[1][N][N] + dp[2][N][N]);
+
+    }
+
 }
