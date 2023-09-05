f23a7b6576409e84991ba37ec7d67768d6089b44
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-09-05 22:15:59 +0900

    [Gold II] Title: 반도체 설계, Time: 188 ms, Memory: 18708 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -2,37 +2,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
 public class Main {
 
-	 public static void main(String[] args) throws Exception {
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 StringTokenizer st;
-		 
-		 int N = Integer.parseInt(br.readLine());
-		 
-		 int arr[] = new int[N];
-		 st = new StringTokenizer(br.readLine());
-		 for(int i=0; i<N; i++) {
-			 arr[i] = Integer.parseInt(st.nextToken());
-		 }
-		 
-		 int lis[] = new int[N];
-		 lis[0] = arr[0];
-		 int idx = 1;
-		 int tmp = 0;
-		 for(int i=1; i<N; i++) {
-			 if(lis[idx-1] < arr[i]) {
-				 lis[idx++] = arr[i];
-			 } else if(lis[0] > arr[i]) {
-				 lis[0] = arr[i];
-			 } else {
-				 tmp = Arrays.binarySearch(lis, 0, idx, arr[i]);
-				 lis[tmp < 0 ? (-tmp-1) : tmp] = arr[i];
-			 }
-		 }
-		 
-		 System.out.println(idx);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int lis[] = new int[n];
+		lis[0] = arr[0];
+		int idx = 1;
+		int temp = 0;
+		for(int i = 1; i<n; i++) {
+			if(lis[idx-1]<arr[i])
+				lis[idx++] = arr[i];
+			else if(lis[0]>arr[i])
+				lis[0] = arr[i];
+			else {
+				temp = Arrays.binarySearch(lis, 0, idx, arr[i]);
+				lis[temp < 0 ? (-temp-1) : temp] = arr[i];
+			}
+		}
+		
+		System.out.println(idx);
 	}
-	 
+
 }
