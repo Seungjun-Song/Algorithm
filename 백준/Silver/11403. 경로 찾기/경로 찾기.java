867f921c389c831995fd852b6148fc8ad211e1c1
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-10-14 20:58:00 +0900

    [Silver I] Title: 경로 찾기, Time: 136 ms, Memory: 13780 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,15 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	static int N;
 	static int[][] map;
+	static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,35 +15,29 @@ public class Main {
 	    
 	    N = Integer.parseInt(br.readLine());
 	    map = new int[N][N];
-	    for(int i=0; i<N; i++) {
-	    	Arrays.fill(map[i], Integer.MAX_VALUE/2);
-	    }
 	    
 	    for(int i=0; i<N; i++) {
 	    	st = new StringTokenizer(br.readLine());
 	    	for(int j=0; j<N; j++) {
-	    		int x = Integer.parseInt(st.nextToken());
-	    		if(x==1)
-	    			map[i][j] = x;
+	    		map[i][j] = Integer.parseInt(st.nextToken());
 	    	}
 	    }
 	    
 	    for(int k=0; k<N; k++) {
 	    	for(int i=0; i<N; i++) {
 	    		for(int j=0; j<N; j++) {
-	    			map[i][j] = Math.min(map[i][j], map[i][k]+map[k][j]); 
+	    			if(map[i][k]==1 && map[k][j]==1)
+	    				map[i][j] = 1; 
 	    		}
 	    	}
 	    }
 	    
 	    for(int i=0; i<N; i++) {
 	    	for(int j=0; j<N; j++) {
-	    		if(map[i][j]==Integer.MAX_VALUE/2)
-	    			System.out.print("0 ");
-	    		else
-	    			System.out.print("1 ");
+	    		sb.append(map[i][j]).append(" ");
 	    	}
-	    	System.out.println();
+	    	sb.append("\n");
 	    }
+	    System.out.println(sb);
     }
 }
