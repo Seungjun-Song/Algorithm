43b3c36323a95450c0d5f3d387e1dcbdd10ff652
seungjun <80227755+seungjun123@users.noreply.github.com>
2023-10-24 09:58:37 +0900

    [Gold V] Title: 소트, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Gold/1083. 소트/소트.java
+++ 백준/Gold/1083. 소트/소트.java
@@ -1,40 +1,44 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();  // 배열 크기
-        int[] nums = new int[n];    // 배열 nums
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
         }
-        int s = scanner.nextInt();  // 교환 횟수
-
-        for (int i = 0; i < n; i++) {
-            // 탐색
-            int maxNum = Integer.MIN_VALUE;
-            int maxIdx = -1;
-            for (int j = i; j < Math.min(n, i + s + 1); j++) {
-                if (nums[j] > maxNum) {
-                    maxNum = nums[j];
-                    maxIdx = j;
-                }
-            }
-            // 교환
-            for (int j = maxIdx; j > i; j--) {
-                int temp = nums[j];
-                nums[j] = nums[j - 1];
-                nums[j - 1] = temp;
-            }
-            // 후처리
-            s -= (maxIdx - i);
-            if (s <= 0) {
-                break;
-            }
+        int S = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<N; i++) {
+        	int mv = 0; // 최대값(maxValue)
+        	int mi = 0; // 최대값 인덱스(maxIndex)
+        	
+        	// 최대값 찾기
+        	for(int j=i; j<Math.min(N, i+S+1); j++) {
+        		if(mv<arr[j]) {
+        			mv = arr[j];
+        			mi = j;
+        		}
+        	}
+        	
+        	// 정렬
+        	for(int j=mi; j>i; j--) {
+    			int temp = arr[j];
+    			arr[j] = arr[j-1];
+    			arr[j-1] = temp;
+        	}
+        	
+        	S -= (mi - i);
+        	if(S<=0) break;
         }
-
-        for (int num : nums) {
-            System.out.print(num + " ");
-        }
+        
+        for(int a : arr) System.out.print(a + " ");
     }
 }
