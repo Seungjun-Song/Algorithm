e44c927d956be7a812ec5871fb219dc5d9a82cac
Seungjun Song <80227755+Seungjun-Song@users.noreply.github.com>
2024-03-23 04:02:33 +0900

    [Silver IV] Title: 스택, Time: 348 ms, Memory: 18108 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,44 +1,35 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
-		int num = 0;
-		for(int i=0; i<N; i++) {
-			String s = br.readLine();
-			if(s.contains("push")) {
-				String temp[] = s.split(" ");
-				s = temp[0];
-				num = Integer.parseInt(temp[1]);
-			}
-			
-			switch(s) {
-			case "push":
-				stack.add(num);
-				break;
-			case "pop":
-				if(stack.isEmpty()) System.out.println(-1);
-				else System.out.println(stack.pop());
-				break;
-			case "size":
-				System.out.println(stack.size());
-				break;
-			case "empty":
-				if(stack.isEmpty()) System.out.println(1);
-				else System.out.println(0);
-				break;
-			case "top":
-				if(stack.isEmpty()) System.out.println(-1);
-				else System.out.println(stack.peek());
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            if(str.length()>5) {
+                stack.push(Integer.parseInt(str.substring(5)));
+            }
+            else {
+                switch (str) {
+                    case "pop":
+                        System.out.println(stack.empty() ? -1 : stack.pop());
+                        break;
+                    case "size":
+                        System.out.println(stack.size());
+                        break;
+                    case "empty":
+                        System.out.println(stack.empty() ? 1 : 0);
+                        break;
+                    case "top":
+                        System.out.println(stack.empty() ? -1 : stack.peek());
+                        break;
+                }
+            }
+        }
+    }
 }
