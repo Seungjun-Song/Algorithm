b6cb8d72673560a19536096cc6f5bfdb161ad99e
Seungjun Song <ssj0187@naver.com>
2024-07-02 19:56:18 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 23300 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,65 +1,49 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
-public class Main
-{
-	static int N;
-	static int K;
-	
-	static int visited[] = new int[100001];
-	
-	// X-1, X+1
-	// 2*X
-	public static void main(String[] args) throws IOException
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String input = br.readLine();
-		String[] inputs = input.split(" ");
-		
-		N = Integer.valueOf(inputs[0]);
-		K = Integer.valueOf(inputs[1]);
-		
-		int result = bfs(N);
-		System.out.println(result);
-	}
+public class Main {
 
-	private static int  bfs(int node)
-	{
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		queue.add(node);
-		int index = node;
-		int n = 0;
-		visited[index] = 1;
-		while (queue.isEmpty() == false)
-		{
-			n = queue.remove();
-			
-			if (n == K)
-			{
-				return visited[n]-1;
-			}
-			
-			if (n-1>=0 && visited[n-1] == 0)
-			{
-				visited[n-1] = visited[n]+1;
-				queue.add(n-1);
-			}
-			if (n+1 <= 100000 && visited[n+1] == 0)
-			{
-				visited[n+1] = visited[n]+1;
-				queue.add(n+1);
-			}
-			if (2*n <= 100000 && visited[2*n] == 0)
-			{
-				visited[2*n] = visited[n] + 1;
-				queue.add(2*n);
-			}
-		}
-		return -1;
-	}
+    private static final int MAX = 100000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(bfs(N, K));
+    }
+
+    private static int bfs(int start, int target) {
+        if (start == target) {
+            return 0;
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0});
+        boolean[] visited = new boolean[MAX + 1];
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int position = cur[0];
+            int time = cur[1];
+
+            if (position == target) {
+                return time;
+            }
+
+            int[] nextPositions = {position + 1, position - 1, position * 2};
+            for (int next : nextPositions) {
+                if (next >= 0 && next <= MAX && !visited[next]) {
+                    visited[next] = true;
+                    queue.add(new int[]{next, time + 1});
+                }
+            }
+        }
+        return -1;
+    }
 }
