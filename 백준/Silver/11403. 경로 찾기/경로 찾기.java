d1d79609c7fecf59ef63b76a3a4aa174a95ae9cd
Seungjun Song <ssj0187@naver.com>
2024-07-06 19:53:42 +0900

    [Silver I] Title: 경로 찾기, Time: 552 ms, Memory: 18044 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,43 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int[][] map;
-	static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st;
-	    
-	    N = Integer.parseInt(br.readLine());
-	    map = new int[N][N];
-	    
-	    for(int i=0; i<N; i++) {
-	    	st = new StringTokenizer(br.readLine());
-	    	for(int j=0; j<N; j++) {
-	    		map[i][j] = Integer.parseInt(st.nextToken());
-	    	}
-	    }
-	    
-	    for(int k=0; k<N; k++) {
-	    	for(int i=0; i<N; i++) {
-	    		for(int j=0; j<N; j++) {
-	    			if(map[i][k]==1 && map[k][j]==1)
-	    				map[i][j] = 1; 
-	    		}
-	    	}
-	    }
-	    
-	    for(int i=0; i<N; i++) {
-	    	for(int j=0; j<N; j++) {
-	    		sb.append(map[i][j]).append(" ");
-	    	}
-	    	sb.append("\n");
-	    }
-	    System.out.println(sb);
+    private static int n;
+    private static int[][] graph;
+    private static boolean[] visited;
+    private static int[][] answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        graph = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        answer = graph.clone();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (graph[i][j] == 1) {
+                    visited = new boolean[n];
+                    visited[j] = true;
+                    dfs(i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(answer[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void dfs(int start, int point) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && graph[point][i] == 1) {
+                visited[i] = true;
+                answer[start][i] = 1;
+                dfs(start, i);
+            }
+        }
     }
 }
