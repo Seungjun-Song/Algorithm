6280a2051eee9b36ef17a89675ddb958ad11abef
Seungjun Song <ssj0187@naver.com>
2024-07-09 15:51:51 +0900

    [Bronze V] Title: 문자와 문자열, Time: 168 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,14 +1,11 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        int i = sc.nextInt();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String S = br.readLine();
-		int i = Integer.parseInt(br.readLine());
-		System.out.println(S.charAt(i-1));
-	}
-
+        System.out.println(S.charAt(i - 1));
+    }
 }
