3b892e60fad2ede156564fbeb37cce6ad5f543ec
Seungjun Song <ssj0187@naver.com>
2024-07-09 16:30:26 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 248 ms, Memory: 18472 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,51 +1,42 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int switch_count = sc.nextInt();
 
-	public static void main(String args[]) throws IOException{
-		
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        
-        int N = Integer.parseInt(br.readLine());
-        int state[] = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-        	state[i] = Integer.parseInt(st.nextToken());
+        int[] switch_arr = new int[switch_count + 1];
+        for (int i = 1; i <= switch_count; i++) {
+            switch_arr[i] = sc.nextInt();
         }
-        int num = Integer.parseInt(br.readLine());
-        for(int i=0; i<num; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int gender = Integer.parseInt(st.nextToken());
-        	int number = Integer.parseInt(st.nextToken());
-        	
-        	if(gender == 1) {
-        		for(int j=0; j<N; j++) {
-        			if((j+1) % number == 0) {
-        				state[j] = state[j] == 0 ? 1 : 0;
-        			}
-        		}
-        	} else if(gender == 2) {
-        		state[number-1] = state[number-1] == 1 ? 0 : 1;
-        		for(int j=1; j<N/2; j++) {
-        			 if(number-j-1 < 0 || number+j-1 >= N) break;
-        			 if(state[number-j-1] == state[number+j-1]) {
-        				 state[number-j-1] = state[number-j-1] == 1 ? 0 : 1;
-        				 state[number+j-1] = state[number+j-1] == 1 ? 0 : 1;
-        			 }
-        			 else break;
-        		}
-        	}
+
+        int student_count = sc.nextInt();
+        for (int i = 0; i < student_count; i++) {
+            int gender = sc.nextInt();
+            int switch_number = sc.nextInt();
+
+            if (gender == 1) {
+                int multiply = 1;
+                while (switch_number * multiply <= switch_count) {
+                    switch_arr[switch_number * multiply] = switch_arr[switch_number * multiply] == 0 ? 1 : 0;
+                    multiply += 1;
+                }
+            } else {
+                switch_arr[switch_number] = switch_arr[switch_number] == 0 ? 1 : 0;
+                int left = switch_number - 1, right = switch_number + 1;
+                while (1 <= left && right <= switch_count
+                    && switch_arr[left] == switch_arr[right]) {
+                    switch_arr[left] = switch_arr[left] == 0 ? 1 : 0;
+                    switch_arr[right] = switch_arr[right] == 0 ? 1 : 0;
+                    left--;
+                    right++;
+                }
+            }
         }
-        for(int i=0; i<state.length; i++) {
-        	System.out.print(state[i] + " ");
-        	if((i+1)%20 == 0) System.out.println();
+
+        for (int i = 1; i <= switch_count; i++) {
+            System.out.print(switch_arr[i] + " ");
+            if (i % 20 == 0) System.out.println();
         }
-    }    
+    }
 }
