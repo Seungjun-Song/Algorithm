189e5994cd65829cad6fdad7ef3e38a481758515
Seungjun Song <ssj0187@naver.com>
2024-07-11 17:28:31 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,63 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int arr[][];
-	static boolean visited[][];
-	static int dx[] = {0, 1, 0, -1};
-	static int dy[] = {1, 0, -1, 0};
 
-	public static void main(String args[]) throws IOException{
+    private static int N;
+    private static int M;
+    private static int[][] maze;
+    private static boolean[][] visited;
+    private static int[] dx = {0, 1, 0, -1};
+    private static int[] dy = {1, 0, -1, 0};
+    private static int answer = 0;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        visited = new boolean[N][M];
-        
-        for(int i=0; i<N; i++) {
-        	String s = br.readLine();
-        	for(int j=0; j<M; j++) {
-        		arr[i][j] = (int) s.charAt(j) - 48;
-        	}
-        }
-        bfs(0, 0);
-        System.out.println(arr[N-1][M-1]);
-    }
-	
-	static void bfs(int x, int y) {
-		Deque<node> queue = new ArrayDeque<>();
-		queue.add(new node(x, y));
-		visited[x][y] = true;
-		
-		while(!queue.isEmpty()) {
-			node q = queue.poll();
-			int nx, ny;
-			for(int i=0; i<4; i++) {
-				nx = q.x + dx[i];
-				ny = q.y + dy[i];
-				
-				if(0<=nx && nx<N && 0<=ny && ny<M && arr[nx][ny]==1 && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					queue.add(new node(nx, ny));
-					arr[nx][ny] = arr[q.x][q.y] + 1;
-				}
-			}
-		}
-    }
-	
-	static class node {
-		int x, y;
 
-		public node(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
+        maze = new int[N + 1][M + 1];
+        for (int i = 1; i <= N; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                maze[i][j] = line.charAt(j - 1) - '0';
+            }
+        }
+        
+        bfs();
+
+        System.out.println(answer);
+    }
+
+    private static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{1, 1, 1});
+        visited = new boolean[N + 1][M + 1];
+        visited[1][1] = true;
+        while (!queue.isEmpty()) {
+            int[] point = queue.poll();
+            int x = point[0];
+            int y = point[1];
+            int cnt = point[2];
+
+            if (x == N && y == M) {
+                answer = cnt;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (1<= nx && nx <= N && 1<= ny && ny <= M && !visited[nx][ny] && maze[nx][ny] == 1) {
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, cnt + 1});
+                }
+            }
+        }
+    }
 }
