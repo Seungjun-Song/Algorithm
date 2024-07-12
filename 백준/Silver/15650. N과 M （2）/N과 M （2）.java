499cb7f47975812a8885a92f1bdaf6d0c44c84f7
Seungjun Song <ssj0187@naver.com>
2024-07-12 19:55:26 +0900

    [Silver III] Title: N과 M (2), Time: 184 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,48 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-	static int N;
-	static int M;
-	
-	static int[] src;
-	static int[] tgt;
 
-	public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        src = new int[N];
-        tgt = new int[M];
-        
-        for(int i=0; i<src.length; i++) {
-        	src[i] = i+1;
-        }
-        
-        comb(0, 0);
+    static int N;
+    static int M;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        dfs(new Stack<>(), new boolean[N + 1], 0);
     }
-	static void comb(int srcIdx, int tgtIdx) {
-		if(tgtIdx == tgt.length) {
-			for(int i : tgt)
-				System.out.print(i + " ");
-			System.out.println();
-			return;
-		}
-		
-		if(srcIdx == src.length) return;
-		tgt[tgtIdx] = src[srcIdx];
-		comb(srcIdx+1, tgtIdx+1);
-		comb(srcIdx+1, tgtIdx+0);
-	}
+
+    private static void dfs(Stack<Integer> stack, boolean[] visited, int number) {
+        if (stack.size() == M) {
+            for (Integer i : stack) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && i > number) {
+                visited[i] = true;
+                stack.push(i);
+                dfs(stack, visited, i);
+                stack.pop();
+                visited[i] = false;
+            }
+        }
+    }
 }
