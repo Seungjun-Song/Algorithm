a8cbb9db6d079b5fdb93b9f1d41745948cf7ee23
Seungjun Song <ssj0187@naver.com>
2024-07-21 19:08:44 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,49 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] num = new int[3];
-	static int[] card;
-	static int sum = 0;
-	static int answer = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String args[]) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		card = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		comb(0, 0);
-		System.out.println(answer);
+        int answer = 0;
+        int closest = 0; // M에 가장 가까운 값을 찾기 위한 변수
+
+        // i, j, k를 모두 조합하여 합계를 계산
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= M && sum > closest) {
+                        closest = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(closest);
     }
-	
-	static void comb(int x, int y) {
-		if(y == 3) {
-			for(int i=0; i<3; i++) sum += num[i];
-
-			if(Math.abs(sum-M) < Math.abs(answer-M)
-					&& sum<=M) {
-
-				answer = sum;
-			}
-			sum = 0;
-			return;
-		}
-		
-		if(x == N) return;
-		num[y] = card[x];
-		comb(x+1, y+1);
-		comb(x+1, y);
-	}
 }
