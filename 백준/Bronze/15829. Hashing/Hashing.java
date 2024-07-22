cdff091c1d20d10b8136ac597d9f37ca95929957
Seungjun Song <ssj0187@naver.com>
2024-07-22 20:28:47 +0900

    [Bronze II] Title: Hashing, Time: 216 ms, Memory: 17732 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,10 +6,15 @@ public class Main {
         int L = sc.nextInt();
         String S = sc.next();
 
-        int answer = 0;
+        long answer = 0;
+        long r = 31;
+        long m = 1234567891;
+        long r_pow = 1;
+
         for (int i = 0; i < L; i++) {
             int x = S.charAt(i) - 'a' + 1;
-            answer += (int) (x * Math.pow(31, i));
+            answer = (answer + x * r_pow) % m;
+            r_pow = (r_pow * r) % m;
         }
 
         System.out.println(answer);
