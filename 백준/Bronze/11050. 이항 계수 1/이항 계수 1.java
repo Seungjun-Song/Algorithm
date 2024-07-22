962f949eb0227955a67b43d2f37f8e8142f84c61
Seungjun Song <ssj0187@naver.com>
2024-07-22 20:46:00 +0900

    [Bronze I] Title: 이항 계수 1, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        System.out.println(factorial(N) / (factorial(K) * factorial(N - K)));
+    }
+
+    private static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
