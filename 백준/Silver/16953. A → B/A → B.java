7c8add51ff65608337e3907da92f481553d41ab6
Seungjun Song <ssj0187@naver.com>
2024-07-31 11:48:49 +0900

    [Silver II] Title: A → B, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -1,35 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static Long minValue = Long.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		Long A = Long.parseLong(st.nextToken());
-		Long B = Long.parseLong(st.nextToken());
-		
-		dfs(A, B, 1);
-		if(minValue == Long.MAX_VALUE) System.out.println(-1);
-		else System.out.println(minValue);
-	}
-	
-	private static void dfs(Long a, Long b, int cnt) {
-		if(a.equals(b)) {
-			if(minValue>cnt) minValue = Long.valueOf(cnt);
-			return;
-		}
-		else if(a > b) {
-			return;
-		}
-		// 2를 곱한다.
-		dfs(a*2, b, cnt+1);
-		// 1을 오른쪽에 추가한다.
-		String s = Long.toString(a);
-		s += 1;
-		a = Long.parseLong(s);
-		dfs(a, b, cnt+1);
-	}
+        int answer = 0;
+        while (B > A) {
+            if (B % 2 == 0) {
+                B = B / 2;
+            } else {
+                String s = String.valueOf(B);
+                if (s.charAt(s.length() - 1) != '1') break;
+                s = s.substring(0, s.length() - 1);
+                B = Integer.parseInt(s);
+            }
+            answer++;
+        }
+
+        System.out.println(A == B ? answer + 1 : -1);
+    }
 }
