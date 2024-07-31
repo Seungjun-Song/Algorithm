a5f070df59211844aa199cf7373d11577815b90a
Seungjun Song <ssj0187@naver.com>
2024-07-31 11:20:26 +0900

    [Gold IV] Title: 플로이드, Time: 664 ms, Memory: 45052 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,48 +1,45 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	
-    	int n = Integer.parseInt(br.readLine());
-    	int m = Integer.parseInt(br.readLine());
-    	
-    	int[][] map = new int[n+1][n+1];
-    	for(int i=1; i<=n; i++) {
-    		for(int j=1; j<=n; j++) {
-    			if(i!=j)
-    				map[i][j] = Integer.MAX_VALUE/2;
-    		}
-    	}
-    	
-    	for(int i=0; i<m; i++) {
-    		st = new StringTokenizer(br.readLine());
-    		int a = Integer.parseInt(st.nextToken());
-    		int b = Integer.parseInt(st.nextToken());
-    		int c = Integer.parseInt(st.nextToken());
-    		
-    		map[a][b] = c < map[a][b] ? c : map[a][b];
-    	}
-    	
-    	for(int k=1; k<=n; k++) {
-    		for(int i=1; i<=n; i++) {
-    			for(int j=1; j<=n; j++) {
-    				map[i][j] = Math.min(map[i][j], map[i][k]+map[k][j]);
-    			}
-    		}
-    	}
-    	
-    	for(int i=1; i<=n; i++) {
-    		for(int j=1; j<=n; j++) {
-    			if(map[i][j] == Integer.MAX_VALUE/2)
-    				System.out.print("0 ");
-    			else
-    				System.out.print(map[i][j] + " ");
-    		}
-    		System.out.println();
-    	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] bus = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) bus[i][j] = 0;
+                else bus[i][j] = Integer.MAX_VALUE / 2;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            bus[a][b] = Math.min(bus[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    bus[i][j] = Math.min(bus[i][j], bus[i][k] + bus[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (bus[i][j] == Integer.MAX_VALUE / 2) System.out.print("0 ");
+                else System.out.print(bus[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
