6f1b564fdaf80bb403ace01effc709f69dfb39e8
Seungjun Song <ssj0187@naver.com>
2024-10-18 05:52:18 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,38 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][3];
 
-        int[][] dp = new int[N + 1][4];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= 3; i++) {
-            dp[1][i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int red = Integer.parseInt(st.nextToken());
+            int green = Integer.parseInt(st.nextToken());
+            int blue = Integer.parseInt(st.nextToken());
 
-        for (int i = 2; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= 3; j++) {
-                int x = Integer.parseInt(st.nextToken());
-
-                if (j == 1) {
-                    dp[i][j] = Math.min(dp[i - 1][2] + x, dp[i - 1][3] + x);
-                } else if (j == 2) {
-                    dp[i][j] = Math.min(dp[i - 1][1] + x, dp[i - 1][3] + x);
-                } else {
-                    dp[i][j] = Math.min(dp[i - 1][1] + x, dp[i - 1][2] + x);
-                }
+            if (i==0) {
+                dp[i][0] = red;
+                dp[i][1] = green;
+                dp[i][2] = blue;
+                continue;
             }
+
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + red;
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + green;
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + blue;
         }
 
         int answer = Integer.MAX_VALUE;
-        for (int i = 1; i <= 3; i++) {
-            answer = Math.min(answer, dp[N][i]);
+        for (int i = 0; i < 3; i++) {
+            if (answer > dp[N-1][i]) answer = dp[N-1][i];
         }
+
         System.out.println(answer);
     }
 }
