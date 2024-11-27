4d68089c30cdb8d83d00afe4114fe29cacccfcc2
Seungjun Song <ssj0187@naver.com>
2024-11-27 12:27:34 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 500 ms, Memory: 37488 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -1,87 +1,98 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N, answer = 0;
-	static int[][] map;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {-1, 0, 1, 0};
-	static int[][] sdx = {{-1, 1, -2, 2, 0, -1, 1, -1, 1}, {-1, -1, 0, 0, 2, 0, 0, 1, 1}, {-1, 1, -2, 2, 0, -1, 1, -1, 1}, {1, 1, 0, 0, -2, 0, 0, -1, -1}};
-	static int[][] sdy = {{1, 1, 0, 0, -2, 0, 0, -1, -1}, {-1, 1, -2, 2, 0, -1, 1, -1, 1}, {-1, -1, 0, 0, 2, 0, 0, 1, 1}, {-1, 1, -2, 2, 0, -1, 1, -1, 1}};
-	static int[] rate = {1, 1, 2, 2, 5, 7, 7, 10, 10};
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		map = new int[N][N];
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		tornado(N/2, N/2);
-		System.out.println(answer);
-	}
-	
-	static void tornado(int x, int y) {
-		int distance = 1;
-		int dir = 0;
-		while(true) {
-			for(int d=0; d<distance; d++) {
-				if(x==0 && y==0) break;
-				x += dx[dir];
-				y += dy[dir];
-				int sand = map[x][y];
-				int a = sand;
-				for(int i=0; i<9; i++) {
-					int nx = x + sdx[dir][i];
-					int ny = y + sdy[dir][i];
-					if(nx<0 || ny<0 || nx>N-1 || ny>N-1) {
-						answer += (int) (sand * (rate[i] * 0.01));
-						a -= (int) (sand * (rate[i] * 0.01));
-						continue;
-					}
-					map[nx][ny] += (int) (sand * (rate[i] * 0.01));
-					a -= (int) (sand * (rate[i] * 0.01));
-				}
-				map[x][y] = 0;
-				if(x+dx[dir]<0 || y+dy[dir]<0 || x+dx[dir]>N-1 || y+dy[dir]>N-1)
-					answer += a;
-				else
-					map[x+dx[dir]][y+dy[dir]] += a;
-			}
-			if(x==0 && y==0) break;
-			dir++;
-			for(int d=0; d<distance; d++) {
-				x += dx[dir];
-				y += dy[dir];
-				int sand = map[x][y];
-				int a = sand;
-				for(int i=0; i<9; i++) {
-					int nx = x + sdx[dir][i];
-					int ny = y + sdy[dir][i];
-					if(nx<0 || ny<0 || nx>N-1 || ny>N-1) {
-						answer += (int) (sand * (rate[i] * 0.01));
-						a -= (int) (sand * (rate[i] * 0.01));
-						continue;
-					}
-					map[nx][ny] += (int) (sand * (rate[i] * 0.01));
-					a -= (int) (sand * (rate[i] * 0.01));
-				}
-				map[x][y] = 0;
-				if(x+dx[dir]<0 || y+dy[dir]<0 || x+dx[dir]>N-1 || y+dy[dir]>N-1)
-					answer += a;
-				else
-					map[x+dx[dir]][y+dy[dir]] += a;
-			}
-			
-			dir = dir+1 > 3 ? 0 : dir+1;
-			distance++;
-		}
-	}
+
+    private static int N;
+    private static int[][] matrix;
+    private static int[][] dx = {{-1, 1, -1, 1, -2, 2, -1, 1, 0, 0}, {1, 1, 0, 0, 0, 0, -1, -1, 2, 1}
+                                ,{-1, 1, -1, 1, -2, 2, -1, 1, 0, 0}, {-1, -1, 0, 0, 0, 0, 1, 1, -2, -1}};
+    private static int[][] dy = {{-1, -1, 0, 0, 0, 0, 1, 1, -2, -1}, {-1, 1, -1, 1, -2, 2, -1, 1, 0, 0}
+                                ,{1, 1, 0, 0, 0, 0, -1, -1, 2, 1}, {-1, 1, -1, 1, -2, 2, -1, 1, 0, 0}};
+    private static int[] weight = {10, 10, 7, 7, 2, 2, 1, 1, 5};
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        matrix = new int[N][N];
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        simulation(N/2, N/2, "left", 1);
+
+        System.out.println(answer);
+    }
+
+    private static void simulation(int r, int c, String direction, int len) {
+        if(len==N) {
+            for(int i=1; i<len; i++) {
+                if(c-i>=0)
+                    move(r, c-i, 0);
+            }
+            return;
+        }
+
+        if(direction.equals("left")) {
+            for(int i=1; i<=len; i++) {
+                if(c-i>=0)
+                    move(r, c-i, 0);
+            }
+            simulation(r, c-len, "down", len);
+        } else if(direction.equals("down")) {
+            for(int i=1; i<=len; i++) {
+                if(r+i<N)
+                    move(r+i, c, 1);
+            }
+            simulation(r+len, c, "right", len+1);
+        } else if(direction.equals("right")) {
+            for(int i=1; i<=len; i++) {
+                if(c+i<N)
+                    move(r, c+i, 2);
+            }
+            simulation(r, c+len, "up", len);
+        } else if(direction.equals("up")) {
+            for(int i=1; i<=len; i++) {
+                if(r-i>=0)
+                    move(r-i, c, 3);
+            }
+            simulation(r-len, c, "left", len+1);
+        }
+    }
+
+    private static void move(int r, int c, int d) {
+        int temp = 0;
+        int nx = 0;
+        int ny = 0;
+
+        for(int i=0; i<10; i++) {
+            nx = r + dx[d][i];
+            ny = c + dy[d][i];
+            if(i==9) break;
+
+            int value = (int) Math.floor((double) matrix[r][c] / 100 * weight[i]);
+            temp += value;
+
+            if(0<=nx && nx<N && 0<=ny && ny<N && value>0) {
+                matrix[nx][ny] += value;
+            } else {
+                answer += value;
+            }
+        }
+
+        matrix[r][c] -= temp;
+        if(0<=nx && nx<N && 0<=ny && ny<N) {
+            matrix[nx][ny] += matrix[r][c];
+        } else {
+            answer += matrix[r][c];
+        }
+        matrix[r][c] = 0;
+    }
 }
